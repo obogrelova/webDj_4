4b1dfdd07a65b8fc0788c8f76c6d90dbf0947ce4
obogrelova <on.obogrelova@gmail.com>
2025-02-02 17:45:55 +0500

    Update files

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -1,7 +1,12 @@
 from .models import Film
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 class FilmForm(ModelForm):
     class Meta:
         model = Film
         fields = ['title', 'description', 'review']
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название фильма'}),
+            'description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите описание фильма'}),
+            'review': Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите отзыв о фильме'})
+        }
